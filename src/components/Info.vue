8c09a33f4cef3448dc7db68275c9aaf55a81cb6e
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-07-12 17:55:12 +0600

    working on slot

---
+++ src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ShadowBox>
+        <template v-slot="">
+          <slot name="shadow-box">
+            
+          </slot>
+        </template>
+      </ShadowBox>
+    </div>
+</template>
+
+<script setup>
+import ShadowBox from './ShadowBox.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
+import RoundedBox from '../components/RoundedBox.vue';
+import Info from '../components/Info.vue';
+
 
 </script>
 
 <template>
-  <main>
+  <main class="container mx-auto bg-gray-50">
     This is homepage
+    <div class="mt-8">
+      <RoundedBox />
+      <Info>
+        <template v-slot:shadow-box>shadow box slot</template>
+      </Info>
+    </div>
   </main>
 </template>
+<style>
+
+</style>
