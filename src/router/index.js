f24622e715ce1852d52a23728a8641ea83319785
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-09-27 15:37:35 +0600

    set static design

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProductsPage from '../views/ProductsPage.vue'
+import ProductDetail from '../views/ProductDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/products',
+      name: 'productPage',
+      component: ProductsPage
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/products/:id',
+      name: 'productDetail',
+      component: ProductDetail
+    },
+    {
+      path: '/cart',
+      name: 'cartPage',
+      component: () => import('../views/CartPage.vue')
+    },
+    {
+      path: '/',
+      redirect: '/products'
+    }
   ]
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,13 @@
-<script setup>
-import RoundedBox from '../components/RoundedBox.vue';
-import Info from '../components/Info.vue';
+<template>
+<div class="container ">
+ 
+</div>
+</template>
 
+<script setup>
 
 </script>
 
-<template>
-  <main class="container mx-auto bg-gray-50">
-    This is homepage
-    <div class="mt-8">
-      <RoundedBox />
-      <Info>
-        <template v-slot:shadow-box>shadow box slot</template>
-      </Info>
-    </div>
-  </main>
-</template>
-<style>
+<style scoped>
 
 </style>
